6831e8b708d03559bd4247e286c7209c0105d5fc
Michael Arakilian <arakilian0@gmail.com>
2019-12-17 17:53:40 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let body = document.querySelector('body')
+let menuPanelTrigger = document.getElementById('menuPanelTrigger')
+let menuPanelQuit = document.getElementById('menuPanelQuit')
+let menuPanel = document.getElementById('menuPanelOverlay')
+
+let contactPanelTrigger = document.getElementById('contactPanelTrigger')
+let contactPanelQuit = document.getElementById('contactPanelQuit')
+let contactPanel = document.getElementById('contactPanelOverlay')
+
+menuPanelTrigger.addEventListener('click', function() {
+  menuPanelTrigger.style.display = "none"
+  menuPanel.style.display = "flex"
+  body.style.overflow = "hidden"
+})
+menuPanelQuit.addEventListener('click', function() {
+  menuPanelTrigger.style.display = "flex"
+  menuPanel.style.display = "none"
+  body.style.overflow = "auto"
+})
+
+contactPanelTrigger.addEventListener('click', function() {
+  contactPanel.style.display = "flex"
+  setTimeout(function() {
+    body.style.overflow = "hidden"
+  }, 400)
+})
+contactPanelQuit.addEventListener('click', function() {
+  contactPanel.style.display = "none"
+  body.style.overflow = "auto"
+})
