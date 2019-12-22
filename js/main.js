495fd13101f7e1f2edf423ab2cd8ceec86808d2e
Michael Arakilian <arakilian0@gmail.com>
2019-12-21 17:02:21 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,56 @@
-let body = document.querySelector('body')
-let menuPanelTrigger = document.getElementById('menuPanelTrigger')
-let menuPanelQuit = document.getElementById('menuPanelQuit')
-let menuPanel = document.getElementById('menuPanelOverlay')
+const items = document.querySelectorAll('.slider-item');
+const itemCount = items.length;
+const nextItem = document.querySelector('.next');
+const previousItem = document.querySelector('.previous');
+const navItem = document.querySelector('a.toggle-nav');
+let count = 0;
 
-let contactPanelTrigger = document.getElementById('contactPanelTrigger')
-let contactPanelQuit = document.getElementById('contactPanelQuit')
-let contactPanel = document.getElementById('contactPanelOverlay')
+function showNextItem() {
+  items[count].classList.remove('active');
 
-menuPanelTrigger.addEventListener('click', function() {
-  menuPanelTrigger.style.display = "none"
-  menuPanel.style.display = "flex"
-  body.style.overflow = "hidden"
-})
-menuPanelQuit.addEventListener('click', function() {
-  menuPanelTrigger.style.display = "flex"
-  menuPanel.style.display = "none"
-  body.style.overflow = "auto"
-})
+  if(count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
 
-contactPanelTrigger.addEventListener('click', function() {
-  contactPanel.style.display = "flex"
-  setTimeout(function() {
-    contactPanelTrigger.style.opacity = "0"
-  }, 100)
-  setTimeout(function() {
-    contactPanelTrigger.style.display = "none"
-    body.style.overflow = "hidden"
-  }, 400)
-})
-contactPanelQuit.addEventListener('click', function() {
-  contactPanel.style.display = "none"
-  body.style.overflow = "auto"
-  contactPanelTrigger.style.opacity = "1"
-  contactPanelTrigger.style.display = "flex"
-})
+  items[count].classList.add('active');
+  console.log(count);
+}
+
+function showPreviousItem() {
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+
+  items[count].classList.add('active');
+  // Check if working...
+  console.log(count);
+}
+
+function toggleNavigation(){
+  this.nextElementSibling.classList.toggle('active');
+}
+
+function keyPress(e) {
+  e = e || window.event;
+
+  if (e.keyCode == '37') {
+    showPreviousItem();
+  } else if (e.keyCode == '39') {
+    showNextItem();
+  }
+}
+
+nextItem.addEventListener('click', showNextItem);
+previousItem.addEventListener('click', showPreviousItem);
+document.addEventListener('keydown', keyPress);
+navItem.addEventListener('click', toggleNavigation);
+
+function openContact() {
+  console.log('opening...')
+}
